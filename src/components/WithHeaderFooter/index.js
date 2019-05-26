8619169f63416cb5ed22c73056d6f878aaba3b1a
Gryffindor <838492657@qq.com>
2019-05-26 13:25:46 +0800

    feat: 初步加入Card

--- src/components/WithHeaderFooter/index.js
+++ src/components/WithHeaderFooter/index.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import styles from './index.module.less';
 import Header from "../Header";
 import Footer from "../Footer";
 
-export default function WithHeaderFooter (WrappedComponent) {
-    return class extends Component {
-        render() {
-            const { ...passThroughProps } = this.props;
-            return (
-                <div className={styles.wholeBody}>
-                    <Header/>
-                    <div className={styles.bodyContent}>
-                        <WrappedComponent {...passThroughProps} />
-                    </div>
-                    <Footer />
-                </div>
-            )
-        }
+export default function WithHeaderFooter(WrappedComponent) {
+  return class extends Component {
+    render() {
+      const {...passThroughProps} = this.props;
+      return (
+          <div className={styles.wholeBody}>
+            <Header/>
+            <div className={styles.bodyContent}>
+              <WrappedComponent {...passThroughProps} />
+            </div>
+            <Footer/>
+          </div>
+      )
     }
+  }
 }
--- src/router/Allgames/index.js
+++ src/router/Allgames/index.js
@@ -3,12 +3,29 @@ import React from "react";
 import styles from "./index.module.less";
 import {NavLink} from "react-router-dom";
 import WithHeaderFooter from "../../components/WithHeaderFooter";
+import {Avatar, Card, Icon} from "antd";
 
 class Allgames extends PureComponent {
   render() {
     return (
         <div className={styles.whole}>
           All games
+          <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                    alt="example"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+          >
+            <Card.Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title="Card title"
+                description="This is the description"
+            />
+          </Card>,
           <NavLink to="/login">
             去登陆
           </NavLink>
