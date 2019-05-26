075aa38e1cf30ed3aeb2d4b23a65952b51a41616
Slytherin <1085684645@qq.com>
2019-05-26 20:25:40 +0800

    fix: 更新默认图片数据

--- src/router/Allrooms/index.js
+++ src/router/Allrooms/index.js
@@ -5,14 +5,14 @@ import {NavLink} from "react-router-dom";
 import WithHeaderFooter from "../../components/WithHeaderFooter";
 import {Card, Row, Col, Icon, Button} from 'antd'
 
-const {Meta}=Card;
+const {Meta} = Card;
 
 const gameInfo = {
     gameName: 'AI 对战五子棋',
     gameIntro: '五子棋是世界智力运动会竞技项目之一，是一种两人对弈的纯策略型棋类游戏，是世界智力运动会竞技项目之一，通常双方分别使用黑白两色的棋子，下在棋盘直线与横线的交叉点上，先形成5子连线者获胜。' +
         '棋具与围棋通用，起源于中国上古时代的传统黑白棋种之一。主要流行于华人和汉字文化圈的国家以及欧美一些地区，是世界上最古老的棋。' +
         '容易上手，老少皆宜，而且趣味横生，引人入胜；不仅能增强思维能力，提高智力，而且富含哲理，有助于修身养性。已在各个游戏平台有应用。',
-    gamePic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558867508883&di=919f63312153b88041b4df1a11887a9f&imgtype=0&src=http%3A%2F%2Fwww.qbaobei.com%2FUploads%2FEditor%2F2015-11-02%2F5636de2320c09.jpg'
+    gamePic: 'https://cdn.nlark.com/yuque/0/2019/png/248245/1558873338221-f29b560c-b606-4fd9-9476-3b173c0c4430.png'
 };
 const roomInfos = [
     {roomId: 1, currentCount: 0, maxCount: 2},
@@ -54,6 +54,7 @@ class Allrooms extends PureComponent {
 
     handleClick = (roomId) => {
         //todo() 进入房间比赛
+        this.props.history.push(`/room/${roomId}`);
         console.log(roomId)
     };
 
@@ -118,11 +119,12 @@ class Allrooms extends PureComponent {
                 >
                     <Card type="inner" title="对战方法介绍"
                           headStyle={{
-                              backgroundImage: "linear-gradient(15deg, rgba(19, 84, 122, 0.72) 0%, rgba(128, 208, 199, 0.56) 100%)",
-                              color: "white"
+                              backgroundImage: "linear-gradient(120deg, rgba(104, 194, 138, 0.2) 0%, rgba(143, 211, 244, 0.18) 100%)",
+                              color: "grey",
+                              letterSpacing: 3
                           }}
                     >
-                        <Row gutter={8}>
+                        <Row gutter={24}>
                             <Col span={18}>
                                 <div className={styles.gameIntro}>
                                     {gameInfo.gameIntro}
@@ -136,8 +138,10 @@ class Allrooms extends PureComponent {
                     </Card>
                     <Card style={{marginTop: 16}} type="inner" title="对战房间"
                           headStyle={{
-                              backgroundImage: "linear-gradient(15deg, rgba(19, 84, 122, 0.72) 0%, rgba(128, 208, 199, 0.56) 100%)",
-                              color: "white"
+                              backgroundImage: "linear-gradient(120deg, rgba(104, 194, 138, 0.2) 0%, rgba(143, 211, 244, 0.18) 100%)",
+                              color: "grey",
+                              letterSpacing: 3
+
                           }}
                     >
 
@@ -146,8 +150,8 @@ class Allrooms extends PureComponent {
                                 return (
                                     <Col span={8}>
 
-                                        <Card  className={styles.roomCard}
-                                              headStyle={{backgroundColor:'transparent'}}>
+                                        <Card className={styles.roomCard}
+                                              headStyle={{backgroundColor: 'transparent'}}>
                                             <Meta
                                                 title={'房间' + room.roomId}
                                             />
@@ -165,7 +169,9 @@ class Allrooms extends PureComponent {
                                                             <Icon type="laptop"
                                                                   style={{fontSize: 80 + 'px', marginTop: 10 + 'px'}}/>
                                                             <div className={styles.availableRoomMsg}>
-                                                                <p> 当前房间人数：<span style={{color:'#68c28a'}}>{room.currentCount}</span></p>
+                                                                <p> 当前房间人数：<span
+                                                                    style={{color: '#68c28a'}}>{room.currentCount}</span>
+                                                                </p>
                                                                 <p> 共可容纳人数：{room.maxCount}</p>
 
                                                             </div>
