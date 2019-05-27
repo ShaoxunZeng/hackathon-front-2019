0e1f3bc0592f94173c0262f607e3ed581e310127
Slytherin <1085684645@qq.com>
2019-05-27 10:17:33 +0800

    feat: websocket请求落子状态

--- src/router/Onplay/index.js
+++ src/router/Onplay/index.js
@@ -41,36 +41,30 @@ class Onplay extends PureComponent {
         //   prepareStatus: true //todo()
         // }
         this.state = {
-            users: [],
-            maps: maps,
-            prepareStatus: true
+            maps: [],
+            prepareStatus: false
         }
     }
 
     componentWillMount() {
-        //todo()  新建websocket请求房间人数 users
-        this.setState({
-            users: ['UserA', 'UserB'],
-            maps: maps
-        });
+        let roomid = this.props.match.params;
+        let {gameName} = this.props.location;
+        let socket = getRoomInfo(gameName, roomid);
+        //打开事件
+        socket.onopen = function () {
+            console.log("Socket 已打开");
+        };
+        //获得消息事件
+        socket.onmessage = function (msg) {
+            console.log(JSON.parse(msg.data));
+            //发现消息进入    开始处理前端触发逻辑
+            //todo() 房间满员后判断两个人的准备状态
+            this.setState({
+                maps: maps,  //todo() test data
+                prepareStatus: true //todo() test data
+            })
+        };
 
-        if (this.state.users.length === 2) {
-            let socket = getRoomInfo(20);
-            //打开事件
-            socket.onopen = function () {
-                console.log("Socket 已打开");
-            };
-            //获得消息事件
-            socket.onmessage = function (msg) {
-                console.log(JSON.parse(msg.data));
-                //发现消息进入    开始处理前端触发逻辑
-                //todo() 房间满员后判断两个人的准备状态
-                this.setState({
-                    maps: maps,  //todo() test data
-                    prepareStatus: true //todo() test data
-                })
-            };
-        }
     }
 
     componentWillUnmount() {
@@ -79,50 +73,14 @@ class Onplay extends PureComponent {
         leaveRoom(gameName, roomid, window.localStorage.username);
     }
 
-    // render() {
-    //   return (
-    //       <div className={styles.whole}>
-    //         {
-    //           this.state.users.length === 2 ?
-    //               <div className={styles.wrapper}>
-    //                 <div className={styles.leftScoreBoard}>
-    //                   <ScoreBoard user={this.state.users[0]} record={this.state.maps[0].positions}
-    //                               avatarColor="linear-gradient(120deg, #157c55 0%, #68c28a 100%)"
-    //                               defaultColor="rgba(128, 208, 199, 0.28)"/>
-    //                 </div>
-    //                 <div className={styles.board}>
-    //                   {this.state.prepareStatus ?
-    //                       <Board maps={this.state.maps}/> :
-    //                       <div className={styles.hintMsg}>对局即将开始</div>
-    //                   }
-    //                 </div>
-    //                 < div className={styles.rightScoreBoard}>
-    //                   <ScoreBoard user={this.state.users[1]} record={this.state.maps[1].positions}
-    //                               avatarColor="linear-gradient(120deg, #13547A 0%, #8fd3f4 100%)"
-    //                               defaultColor="rgba(148, 221, 255, 0.35)"/>
-    //                 </div>
-    //               </div> :
-    //               <div className={styles.waitStatus}>
-    //                 <Board maps={maps}/>
-    //                 <div className={styles.cover}>
-    //                   <div className={styles.msg}>
-    //                     <div>等待其他玩家加入…</div>
-    //                     {/*todo() UI modify*/}
-    //                     <div>
-    //                       <Icon
-    //                           style={{fontSize: 100 + 'px', marginTop: 10 + 'px', color: '#68c28a'}}
-    //                           type="loading"/>
-    //                     </div>
-    //                   </div>
-    //                 </div>
     render() {
         return (
             <div className={styles.whole}>
                 {
-                    this.state.users.length === 2 ?
+                    this.state.maps.length === 2 ?
                         <div className={styles.wrapper}>
                             <div className={styles.leftScoreBoard}>
-                                <ScoreBoard user={this.state.users[0]} record={this.state.maps[0].positions}
+                                <ScoreBoard user={this.state.maps[0].username} record={this.state.maps[0].positions}
                                             avatarColor="linear-gradient(120deg, #157c55 0%, #68c28a 100%)"
                                             defaultColor="rgba(128, 208, 199, 0.28)"/>
                             </div>
@@ -141,7 +99,7 @@ class Onplay extends PureComponent {
                                 }
                             </div>
                             < div className={styles.rightScoreBoard}>
-                                <ScoreBoard user={this.state.users[1]} record={this.state.maps[1].positions}
+                                <ScoreBoard user={this.state.maps[1].username} record={this.state.maps[1].positions}
                                             avatarColor="linear-gradient(120deg, #13547A 0%, #8fd3f4 100%)"
                                             defaultColor="rgba(148, 221, 255, 0.35)"/>
                             </div>
@@ -151,7 +109,6 @@ class Onplay extends PureComponent {
                             <div className={styles.cover}>
                                 <div className={styles.msg}>
                                     <div>等待其他玩家加入…</div>
-                                    {/*todo() UI modify*/}
                                     <div>
                                         <Icon
                                             style={{fontSize: 100 + 'px', marginTop: 10 + 'px', color: '#68c28a'}}
@@ -168,7 +125,8 @@ class Onplay extends PureComponent {
             </div>
 
         )
-    };
+    }
+    ;
 }
 
 export default WithHeaderFooter(Onplay);
--- src/services/apiWS.js
+++ src/services/apiWS.js
@@ -1,5 +1,5 @@
 const SERVER_LOC = "192.168.162.194:9001";
 
-export const getRoomInfo = (rId) => {
-  return new WebSocket(`ws://${SERVER_LOC}/client/${rId}`);
+export const getRoomInfo = (gId, rId) => {
+  return new WebSocket(`ws://${SERVER_LOC}/${gId}/${rId}`);
 };
